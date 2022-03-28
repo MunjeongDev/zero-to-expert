93f73cf0746c64bcd21e680370619f0f9fdb2212
Munjeong Choi <munjeong.dev@gmail.com>
2022-03-28 11:22:54 +0900

    Fundamentals: Array

--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -79,7 +79,7 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
-*/
+
 
 const calcAge = function (birthYear) {
   return 2022 - birthYear;
@@ -100,3 +100,129 @@ const yearUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearUntilRetirement(1996, "Munjeong"));
 console.log(yearUntilRetirement(1950, "Mike"));
+
+
+// Coding Challenge #1
+
+Back to the two gymnastics teams, the Dolphins and Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 AND DATA 2.
+5. Ignore draws this time
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+*/
+
+/* 미해결
+const calcAverage = (round1, round2, round3) => (round1 + round2 + round3) / 3;
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+console.log(avgDolphins);
+console.log(avgKoalas);
+
+const checkWinner = (avgDolphins, avgKoalas) => 
+if (avgDolphins > avgKoalas) {
+  console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+} else if ((avgDolphins = avgKoalas)) {
+  console.log("Draws");
+} else {
+  console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+} 
+
+// Array
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // 0부터 시작하니까
+
+friends[2] = "Heejin";
+console.log(friends);
+
+const firstName = "Munjeong";
+const munjeong = [firstName, "Choi", 2022 - 1996, "web developer", friends];
+console.log(munjeong);
+console.log(munjeong.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]),calcAge(years[1]),calcAge(years[years.length - 1])];
+console.log(ages); 
+
+const friends = ["Michael", "Steven", "Heejin"];
+// Add elements
+const newLength = friends.push("Shuhwa");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); //last
+const popped = friends.pop(); // last
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // first
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes("23"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
+
+// Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is differnet, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+// const calcTip = bill => bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
