7da365421141c7b932a7f8ef4e25ef9d91a4ff98
Munjeong Choi <munjeong.dev@gmail.com>
2022-04-05 10:24:21 +0900

    Scoping in Practice

---
+++ Behind-the-Scenes/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = "Steven";
+
+      // Reassigning outer scope's variable
+      output = "NEW OUTPUT!";
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = "Munjeong";
+calcAge(1996);
+
+// console.log(age);
+// printAge():
